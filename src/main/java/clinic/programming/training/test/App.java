afda1d629267ac35a4e3e9a61779ea476e1aaf7d
Eduard <anemadevelop@gmail.com>
2020-05-05 04:19:59 +0200

    second commit

---
+++ src/main/java/clinic/programming/training/test/App.java
@@ -0,0 +1,14 @@
+package clinic.programming.training.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
